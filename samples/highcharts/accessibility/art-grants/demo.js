c5f65d714a40107c39a4258204bff7450a799bd8
oysteinmoseng <oystein@highsoft.com>
2017-05-18 10:16:29 +0200

    Fixed issue with art-grants demo after master merge.

--- samples/highcharts/accessibility/art-grants/demo.js
+++ samples/highcharts/accessibility/art-grants/demo.js
@@ -344,9 +344,3 @@ Highcharts.chart('container', {
         color: colors[3]
     }]
 });
-
-// Remove click events on container to avoid having "clickable" announced by AT
-// These events are needed for custom click events, drag to zoom, and navigator
-// support.
-delete chart.container.onmousedown;
-delete chart.container.onclick;
